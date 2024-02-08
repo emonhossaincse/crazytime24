5222a6516e259215d61da9c6c0c404506ba105b8
Emon Hossain <55936127+emonhossaincse@users.noreply.github.com>
2024-02-08 23:18:10 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,62 +1,49 @@
-import React, { useState } from 'react';
+import logo from './logo.svg';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import './assets/css/main.css';
+import './assets/css/defult.css';
+import './assets/css/footer.css';
+import './assets/css/modal.css';
+import './assets/css/singup.css';
+import Aside from './component/nav/Aside';
+import TopNavMenu from './component/nav/TopNavMenu';
+import Chat from './component/sidebar/Chat';
+import HomeBigBanner from './component/banner/HomeBigBanner';
+import HomeSlider from './component/slider/HomeSlider';
+import RecentWins from './component/slider/RecentWins';
+import TopRatedGames from './component/slider/TopRatedGames';
+import PaymentBanner from './component/banner/PaymentBanner';
+import HomeTab from './component/tabbar/HomeTab';
+import ExclusiveBanner from './component/banner/ExclusiveBanner';
+import Footer from './layout/Footer';
+import Category from './layout/Category';
+import YourComponent from './component/modal/modal';
+import Login from './layout/Test';
+
+
+
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [nickname, setNickname] = useState('');
-
-  const createPlayer = async () => {
-    const apiUrl = 'https://stage.game-program.com/api/seamless/provider';
-
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        api_password: 'CQyE62YHH2ygbPd1Cb',
-        api_login: 'six6_mc_s',
-        method: 'createPlayer',
-        user_username: username,
-        user_password: password,
-        user_nickname: nickname,
-        currency: 'EUR',
-      }),
-    };
-
-    try {
-      const response = await fetch(apiUrl, requestOptions);
-      const responseData = await response.json();
-
-      if (responseData.error === 0) {
-        console.log('Player created successfully:', responseData.response);
-      } else {
-        console.error('Error creating player:', responseData.message);
-      }
-    } catch (error) {
-      console.error('Error making API request:', error);
-    }
-  };
-
+  
   return (
-    <div>
-      <h1>Create Player</h1>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Nickname:
-        <input type="text" value={nickname} onChange={(e) => setNickname(e.target.value)} />
-      </label>
-      <br />
-      <button onClick={createPlayer}>Create Player</button>
+    <div className="App">
+      <Aside/>
+      <Chat/>
+      <TopNavMenu/>
+      <HomeBigBanner/>
+      <HomeSlider/>
+      <Category/>
+      <RecentWins/>
+      <TopRatedGames/>
+      <PaymentBanner/>
+      <HomeTab/>
+      <ExclusiveBanner/>
+      <Footer/>
+           
+         
     </div>
   );
 }
