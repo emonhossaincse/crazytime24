3fb7b9350879b5277948da035cb31bf9a189601f
Emon Hossain <55936127+emonhossaincse@users.noreply.github.com>
2024-02-22 11:28:24 +0600

    update

--- src/pages/Sports.js
+++ src/pages/Sports.js
@@ -1,37 +1,18 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 const Sports = () => {
   const location = useLocation();
-  const { sportsLink } = location.state || {};
-  const containerRef = useRef(null);
+  const { soprtsLink } = location.state || {}; 
+  
 
-  useEffect(() => {
-    if (!sportsLink || !containerRef.current) return;
-
-    // Clear previous content
-    containerRef.current.innerHTML = sportsLink;
-
-    // Find and execute scripts (both inline and external)
-    const scripts = [...containerRef.current.getElementsByTagName("script")];
-    scripts.forEach((oldScript) => {
-      const newScript = document.createElement("script");
-      Array.from(oldScript.attributes).forEach(attr => newScript.setAttribute(attr.name, attr.value));
-      
-      // For inline scripts
-      if (!oldScript.src) {
-        newScript.textContent = oldScript.textContent;
-      }
-
-      oldScript.parentNode.replaceChild(newScript, oldScript);
-    });
-
-  }, [sportsLink]);
 
   return (
     <div className='max-width game-lunch'>
-      <div className="container" ref={containerRef}>
-        {/* Content and scripts injected here */}
+      <div className="container">
+            
+        {/* <iframe src='https://six6.site/game'/> */}
+        <div dangerouslySetInnerHTML={{ __html: soprtsLink}} />
       </div>
     </div>
   );
