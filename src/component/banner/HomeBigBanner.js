4edf049ea846481643edb31dbfbe18fcb5522b83
Emon Hossain <55936127+emonhossaincse@users.noreply.github.com>
2024-11-30 01:29:36 +0600

    upload

--- src/component/banner/HomeBigBanner.js
+++ src/component/banner/HomeBigBanner.js
@@ -91,11 +91,11 @@ function HomeBigBanner() {
             <div className="thumbnail col-lg-7">
               <img
                 className="light-mode"
-                src="/assets/media/thumbnail/169018078930595.png"
+                src="/assets/media/thumbnail/.png"
               />
               <img
                 className="dark-mode"
-                src="/assets/media/thumbnail/16901807926590.png"
+                src="/assets/media/thumbnail/.png"
               />
             </div>
           </div>
