ad29e8654a9142177d4b67c27b3207f78f7acea0
Emon Hossain <55936127+emonhossaincse@users.noreply.github.com>
2024-12-18 04:26:04 +0600

    upload

--- src/pages/Casino.js
+++ src/pages/Casino.js
@@ -1,11 +1,13 @@
 import React from "react";
 import WhatsNew from "../component/common/WhatsNew";
 import CasinoFooter from "../component/common/CasinoFooter";
+import CasinoNav from "../component/nav/CasinoNav";
 
 function Casino() {
   return (
     <main className="max-width">
       <div className="casino-page">
+        <CasinoNav/>
             
         <WhatsNew />
         
